f3cd40ec1e968265b3b0ad43167222a530474e50
chat43CAAG <153575347+chat43CAAG@users.noreply.github.com>
2024-01-08 20:45:54 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,17 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+apiKey: "AIzaSyD_Sob1iMtn3UiBYlS0IpOTgwBr_AebL_w",
+authDomain: "appweb18dic.firebaseapp.com",
+projectId: "appweb18dic",
+storageBucket: "appweb18dic.appspot.com",
+messagingSenderId: "1030837720384",
+appId: "1:1030837720384:web:126869be3f5b7f0b1616a5"
+});
